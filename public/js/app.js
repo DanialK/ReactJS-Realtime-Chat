8a68a4a28f4989c6aa70b8801874b83356bf99f2
Midhun Manohar <midhun@toobler.com>
2015-05-04 19:04:24 +0530

    improved

---
+++ public/js/app.js
@@ -0,0 +1,225 @@
+/**
+ * @jsx React.DOM
+ */
+var socket = io.connect();
+
+var Users = [];
+var Messages = [];
+
+var UsersList = React.createClass({
+	render: function(){
+		var renderUser = function(user, index){
+			return <li id={index}> { user} </li>
+		};
+		return (
+			<div class='users'>
+				<h3> Online Users </h3>
+				<ul>{ this.props.users.map(renderUser)} </ul>				
+			</div>
+		);
+	}
+});
+
+var Message = React.createClass({
+	render: function(){
+		return(
+			<div class="message-body">
+				<div class="user">{this.props.user}</div>
+				<div class="message">{this.props.text}</div>
+			</div>
+		)
+	}
+});
+
+var MessageList = React.createClass({
+	render: function(){
+		var renderMessage = function(message){
+			return <Message user={message.user} text={message.text} />
+		}
+		return (
+			<div class='messages'>
+				<h2> ... </h2>
+				{ this.props.messages.map(renderMessage)} 
+			</div>
+		);
+	}
+});
+
+var MessageForm = React.createClass({
+
+	getInitialState: function(){
+		return {text: ''};
+	},
+
+	handleSubmit : function(e){
+		e.preventDefault();
+        if(this.state.text) {
+            var message = {
+                user: this.props.user,
+                text: this.state.text
+            }
+            this.props.onMessageSubmit(message);
+            this.setState({text: ''});
+        }
+	},
+
+    handleEnter: function(e){
+        e.preventDefault();
+        if(e.keyCode === 13 && this.state.text && (!e.shiftKey)){
+            var message = {
+                user: this.props.user,
+                text: this.state.text
+            }
+            this.props.onMessageSubmit(message);
+            this.setState({text: ''});
+        }
+    },
+
+	changeHandler : function(e){
+		this.setState({ text : e.target.value });
+	},
+
+	render: function(){
+		return(
+			<div class='message_form'>
+				<h3></h3>
+				<form onKeyUp={this.handleEnter}>
+					<textarea onChange={this.changeHandler} value={this.state.text}></textarea>
+				</form>
+			</div>
+		);
+	}
+});
+
+var ChangeNameForm = React.createClass({
+	getInitialState: function(){
+		return {newName: 'Your Name?'};
+	},
+
+	onKey : function(e){
+		this.setState({ newName : e.target.value });
+	},
+
+    oldName: "",
+
+	handleSubmit : function(e){
+        e.preventDefault();
+        var newName = this.state.newName;
+        if(newName !== this.oldName) {
+            this.props.onChangeName(newName);
+            this.oldName = newName;
+        }
+	},
+
+    onInit: function(e){
+        e.preventDefault();
+        if(this.state.newName == "Your Name?") {
+            this.setState({newName: ''});
+        }
+    },
+
+	render: function(){
+		return(
+			<div class='change_name_form'>
+				<h3></h3>
+				<form onSubmit={this.handleSubmit}>
+					<input onChange={this.onKey} onClick={this.onInit} onBlur={this.handleSubmit} value={this.state.newName} />
+				</form>	
+			</div>
+		);
+	}
+});
+
+var ChatApp = React.createClass({
+
+	getInitialState: function(){
+
+		socket.on('init', this.initialize);
+		socket.on('send:message', this.messageRecieve);
+		socket.on('user:join', this.userJoined);
+		socket.on('user:left', this.userLeft);
+		socket.on('change:name', this.userChangedName);
+
+		return {users: [], messages:[], text: ''};
+	},
+
+	initialize: function(data){
+		this.setState({ users: data.users, user: data.name+'#'+data.users.length});
+	},
+
+	messageRecieve: function(message){
+		this.state.messages.push(message);
+		this.setState();
+	},
+
+	userJoined: function(data){
+		this.state.users.push(data.name);
+		this.state.messages.push({
+			user: 'APLICATION BOT',
+			text : data.name +' Joined'
+		});
+		this.setState();
+	},
+
+	userLeft: function(data){
+		var index = this.state.users.indexOf(data.name);
+		this.state.users.splice(index, 1);
+		this.state.messages.push({
+			user: 'APPLICATION BOT',
+			text : data.name +' Left'
+		});
+		this.setState();
+
+	},
+
+	userChangedName : function(data){
+		var oldName = data.oldName;
+		var newName = data.newName;
+		this.state.users.splice(this.state.users.indexOf(oldName), 1, newName);
+		this.state.messages.push({
+			user: 'APLICATION BOT',
+			text : 'Change Name : ' + oldName + ' ==> '+ newName
+		});
+		this.setState();
+
+	},
+
+	handleMessageSubmit : function(message){
+		this.state.messages.push(message);
+		this.setState();
+
+		socket.emit('send:message', message);
+	},
+
+	handleChangeName : function(newName){
+		var that = this;
+		var oldName = this.state.user;
+		socket.emit('change:name', { name : newName}, function(result){
+			if(!result){
+				alert('There was an error changing your name');
+			}else{
+				var index = that.state.users.indexOf(oldName);
+				that.state.users.splice(index, 1, newName);
+                that.state.user = newName;
+				that.setState();
+			}
+		});
+	},
+
+	render : function(){
+		return (
+			<div class="pull-left">
+                <div class="pull-left">
+				<UsersList users={this.state.users} />
+				<MessageList messages={this.state.messages} />
+                </div>
+                <div class="pull-left">
+				<MessageForm onMessageSubmit={this.handleMessageSubmit} user={this.state.user} />
+				<ChangeNameForm onChangeName={this.handleChangeName} />
+                </div>
+			</div>
+		);
+	}
+});
+
+React.renderComponent(<ChatApp/>, document.body);
---
+++ routes/socket.js
@@ -0,0 +1,100 @@
+// Keep track of which names are used so that there are no duplicates
+var userNames = (function () {
+  var names = {};
+
+  var claim = function (name) {
+    if (!name || names[name]) {
+      return false;
+    } else {
+      names[name] = true;
+      return true;
+    }
+  };
+
+  // find the lowest unused "guest" name and claim it
+  var getGuestName = function () {
+    var name,
+      nextUserId = 1;
+
+    do {
+      name = 'Guest ' + nextUserId;
+      nextUserId += 1;
+    } while (!claim(name));
+
+    return name;
+  };
+
+  // serialize claimed names as an array
+  var get = function () {
+    var res = [];
+    for (user in names) {
+      res.push(user);
+    }
+
+    return res;
+  };
+
+  var free = function (name) {
+    if (names[name]) {
+      delete names[name];
+    }
+  };
+
+  return {
+    claim: claim,
+    free: free,
+    get: get,
+    getGuestName: getGuestName
+  };
+}());
+
+// export function for listening to the socket
+module.exports = function (socket) {
+  var name = userNames.getGuestName();
+
+  // send the new user their name and a list of users
+  socket.emit('init', {
+    name: name,
+    users: userNames.get()
+  });
+
+  // notify other clients that a new user has joined
+  socket.broadcast.emit('user:join', {
+    name: name
+  });
+
+  // broadcast a user's message to other users
+  socket.on('send:message', function (data) {
+    socket.broadcast.emit('send:message', {
+      user: name,
+      text: data.text
+    });
+  });
+
+  // validate a user's name change, and broadcast it on success
+  socket.on('change:name', function (data, fn) {
+    if (userNames.claim(data.name)) {
+      var oldName = name;
+      userNames.free(oldName);
+
+      name = data.name;
+      
+      socket.broadcast.emit('change:name', {
+        oldName: oldName,
+        newName: name
+      });
+
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+
+  // clean up when a user leaves, and broadcast it to other users
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('user:left', {
+      name: name
+    });
+    userNames.free(name);
+  });
+};
