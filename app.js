9121b387e0e54b8c508c7eb901f685c7fcf4f6f2
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-10-07 19:15:22 +0530

    #5 Solved. Chat History For Users Who Join The Room Implemented

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
-'use strict';
+"use strict";
 
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var http = require('http');
+var express = require("express");
+var http = require("http");
 
-var socket = require('./routes/socket.js');
+//Make User History Model using mongodb
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/chatDb";
+
+var history;
+
+MongoClient.connect(url, function(err, db) {
+  useNewUrlParser: true;
+  if (err) {
+    console.log("Error Occured while connecting to the database server");
+  } else {
+    var dbase = db.db("chatDb");
+    history = dbase.collection("Chat History");
+    console.log("Connection Established With Chat History Db To Get History");
+  }
+});
+
+var socket = require("./routes/socket.js");
 
 var app = express();
 var server = http.createServer(app);
 
 /* Configuration */
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-app.set('port', 3000);
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+app.set("port", 3000);
 
-if (process.env.NODE_ENV === 'development') {
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+if (process.env.NODE_ENV === "development") {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
+//Get Room's Chat History
+app.get("/chatHistory", function(req, res) {
+  console.log("End point Called!!");
+  history.findOne({ chatRoom: "1" }, function(err, result) {
+    if (err) {
+      console.log("Sorry some error occured");
+    } else {
+      var current_chat = [];
+
+      if (result != null) {
+        console.log(result.history);
+        current_chat = result.history;
+      }
+
+      res.send({
+        chat_history: current_chat
+      });
+    }
+  });
+});
+
 /* Socket.io Communication */
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', socket);
+var io = require("socket.io").listen(server);
+io.sockets.on("connection", socket);
 
 /* Start server */
-server.listen(app.get('port'), function (){
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+server.listen(app.get("port"), function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
 });
 
 module.exports = app;
--- routes/socket.js
+++ routes/socket.js
@@ -1,8 +1,29 @@
+//Make User History Model using mongodb
+
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/chatDb";
+
+var history;
+
+MongoClient.connect(url, function(err, db) {
+  useNewUrlParser: true;
+  if (err) {
+    console.log("Error Occured while connecting to the database server");
+  } else {
+    var dbase = db.db("chatDb");
+    history = dbase.collection("Chat History");
+    exports.history = history;
+    console.log(
+      "Connection Established With Chat History Db For Add Chat To DB"
+    );
+  }
+});
+
 // Keep track of which names are used so that there are no duplicates
-var userNames = (function () {
+var userNames = (function() {
   var names = {};
 
-  var claim = function (name) {
+  var claim = function(name) {
     if (!name || names[name]) {
       return false;
     } else {
@@ -12,12 +33,12 @@ var userNames = (function () {
   };
 
   // find the lowest unused "guest" name and claim it
-  var getGuestName = function () {
+  var getGuestName = function() {
     var name,
       nextUserId = 1;
 
     do {
-      name = 'Guest ' + nextUserId;
+      name = "Guest " + nextUserId;
       nextUserId += 1;
     } while (!claim(name));
 
@@ -25,7 +46,7 @@ var userNames = (function () {
   };
 
   // serialize claimed names as an array
-  var get = function () {
+  var get = function() {
     var res = [];
     for (user in names) {
       res.push(user);
@@ -34,7 +55,7 @@ var userNames = (function () {
     return res;
   };
 
-  var free = function (name) {
+  var free = function(name) {
     if (names[name]) {
       delete names[name];
     }
@@ -46,40 +67,87 @@ var userNames = (function () {
     get: get,
     getGuestName: getGuestName
   };
-}());
+})();
 
 // export function for listening to the socket
-module.exports = function (socket) {
+module.exports = function(socket) {
   var name = userNames.getGuestName();
 
-  // send the new user their name and a list of users
-  socket.emit('init', {
+  // send the new user their name, list of users and the room history
+  socket.emit("init", {
     name: name,
     users: userNames.get()
   });
 
   // notify other clients that a new user has joined
-  socket.broadcast.emit('user:join', {
+
+  socket.broadcast.emit("user:join", {
     name: name
   });
 
-  // broadcast a user's message to other users
-  socket.on('send:message', function (data) {
-    socket.broadcast.emit('send:message', {
+  socket.on("send:message", function(data) {
+    //Find If Room Id Is Present
+    history.findOne({ chatRoom: "1" }, function(err, result) {
+      if (err) {
+        console.log("Sorry some error occured");
+      } else {
+        var current_chat = [];
+
+        if (result != null) {
+          console.log(result.history);
+          current_chat = result.history;
+        } else {
+          //If room id not present then create a new one
+          console.log("No Room Found , therefore creating a new one");
+          history.insertOne({ chatRoom: "1", history: [] }, function(
+            err,
+            result
+          ) {
+            if (err) {
+              console.log("Error in creating chat room with id 1");
+            } else {
+              console.log("Successfully Created Chat Room With Id 1!!");
+            }
+          });
+        }
+
+        //Create history array
+        current_chat.push({
+          user: name,
+          message: data.text
+        });
+
+        //Update Room History
+        history.updateOne(
+          { chatRoom: "1" },
+          { $set: { history: current_chat } },
+          function(err, result) {
+            if (err) {
+              console.log("Sorry Some error occured while updating");
+            } else {
+              console.log("Successfully Updated room history!!");
+            }
+          }
+        );
+      }
+    });
+
+    // broadcast a user's message to other users
+    socket.broadcast.emit("send:message", {
       user: name,
       text: data.text
     });
   });
 
   // validate a user's name change, and broadcast it on success
-  socket.on('change:name', function (data, fn) {
+  socket.on("change:name", function(data, fn) {
     if (userNames.claim(data.name)) {
       var oldName = name;
       userNames.free(oldName);
 
       name = data.name;
-      
-      socket.broadcast.emit('change:name', {
+
+      socket.broadcast.emit("change:name", {
         oldName: oldName,
         newName: name
       });
@@ -91,8 +159,8 @@ module.exports = function (socket) {
   });
 
   // clean up when a user leaves, and broadcast it to other users
-  socket.on('disconnect', function () {
-    socket.broadcast.emit('user:left', {
+  socket.on("disconnect", function() {
+    socket.broadcast.emit("user:left", {
       name: name
     });
     userNames.free(name);
